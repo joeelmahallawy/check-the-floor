25f4740e8e78a700bc3a009c4659118908bd673e
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-04-20 18:51:35 -0400

    fc

--- helpers/notify.ts
+++ helpers/notify.ts
@@ -2,48 +2,52 @@ import prisma from "../lib/prisma";
 import SendSMS from "./twilio";
 
 const notify = async () => {
-  // get all subscriptions
-  const subscriptions = await prisma.subscribersAndCollections.findMany({
-    select: {
-      triggerPrice: true,
-      Collection: { select: { collectionName: true, id: true } },
-      Subscriber: { select: { phoneNumber: true, id: true } },
-    },
-  });
+  try {
+    // get all subscriptions
+    const subscriptions = await prisma.subscribersAndCollections.findMany({
+      select: {
+        triggerPrice: true,
+        Collection: { select: { collectionName: true, id: true } },
+        Subscriber: { select: { phoneNumber: true, id: true } },
+      },
+    });
+    // get collection name and trigger price of subscribed users
+    const subscriptionData = subscriptions.map((sub) => ({
+      collectionID: sub.Collection.id.toString(),
+      subscriberID: sub.Subscriber.id.toString(),
+      triggerPrice: sub.triggerPrice,
+      collectionName: sub.Collection.collectionName,
+      phoneNumber: sub.Subscriber.phoneNumber.toString(),
+    }));
 
-  // get collection name and trigger price of subscribed users
-  const subscriptionData = subscriptions.map((sub) => ({
-    collectionID: sub.Collection.id.toString(),
-    subscriberID: sub.Subscriber.id.toString(),
-    triggerPrice: sub.triggerPrice,
-    collectionName: sub.Collection.collectionName,
-    phoneNumber: sub.Subscriber.phoneNumber.toString(),
-  }));
-
-  // look thru all subscriptions
-  subscriptionData.forEach(async (sub) => {
-    // check floor price
-    const response = await fetch(
-      `https://api.opensea.io/api/v1/collection/${sub.collectionName}/stats`
-    );
-    const { stats } = await response.json();
-    // if it's less than their trigger price
-    if (stats.floor_price <= sub.triggerPrice) {
-      // send them a message
-      await SendSMS(
-        sub.phoneNumber,
-        `CheckTheFloor: ${sub.collectionName}'s floor price fell under your trigger price (${sub.triggerPrice}ETH), it's currently at ${stats.floor_price} ETH.`
+    // look thru all subscriptions
+    subscriptionData.forEach(async (sub) => {
+      // check floor price
+      const response = await fetch(
+        `https://api.opensea.io/api/v1/collection/${sub.collectionName}/stats`
       );
-      // remove that subscription from the database so they receive message once and don't get spammed
-      await prisma.subscribersAndCollections.delete({
-        where: {
-          collectionID_subscriberID: {
-            collectionID: BigInt(sub.collectionID),
-            subscriberID: BigInt(sub.subscriberID),
+      const { stats } = await response.json();
+      // if it's less than their trigger price
+      if (stats.floor_price <= sub.triggerPrice) {
+        // send them a message
+        await SendSMS(
+          sub.phoneNumber,
+          `CheckTheFloor: ${sub.collectionName}'s floor price fell under your trigger price (${sub.triggerPrice}ETH), it's currently at ${stats.floor_price} ETH.`
+        );
+        // remove that subscription from the database so they receive message once and don't get spammed
+        await prisma.subscribersAndCollections.delete({
+          where: {
+            collectionID_subscriberID: {
+              collectionID: BigInt(sub.collectionID),
+              subscriberID: BigInt(sub.subscriberID),
+            },
           },
-        },
-      });
-    }
-  });
+        });
+      }
+    });
+    return {};
+  } catch (error) {
+    return error;
+  }
 };
 export default notify;
--- pages/api/twilio.ts
+++ pages/api/twilio.ts
@@ -1,18 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { QuirrelClient } from "quirrel/dist/esm/src/client";
+import notify from "../../helpers/notify";
 import SendSMS from "../../helpers/twilio";
 import prisma from "../../lib/prisma";
 import JobQueue from "../api/queues/jobs";
 
-// const quirrelClient = new QuirrelClient({ config: {} });
-
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     // const x = 1800;
-    const x = 30;
-    // auth for only developer to call this (THIS IS TO BE CALLED ONCE)
-    // this calls a job every x seconds (x=10)
-    // whenever a job gets called, we loop thru our database and send messages
+    const x = 10;
+    // auth for developer to call this (THIS IS TO BE CALLED ONCE)
+    // this queues a job every x seconds
+    // whenever a job gets queued, we loop thru our database and send messages
     if (
       // can find this in .env.local
       req.headers.authorization == process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
@@ -22,7 +21,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         {},
         { repeat: { every: x * 1000 } }
       );
-
+      // const not = await notify();
       res.send(queueRes);
     } else {
       throw new Error("Unauthorized");
