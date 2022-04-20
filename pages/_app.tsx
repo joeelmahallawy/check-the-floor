bd1e75f61cae9f551bcb0557ea1c1afba6e930c0
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-04-20 19:13:29 -0400

    added GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import Script from "next/script";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider theme={theme}>
+    {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+    <Script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-226634693-1"
+    ></Script>
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-226634693-1');
+        `}
+    </Script>
     <Component {...pageProps} />
   </ChakraProvider>
 );
